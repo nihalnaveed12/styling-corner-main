efb251b3a51782e43e5f4d9df1efe8041860d1ac
nihalnaveed12 <nihalnaveed50@gmail.com>
2025-06-11 19:51:04 +0500

    sec-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Brouchst from "./components/brouchst";
 import Hero from "./components/Hero";
 import Navbar from "./components/navbar";
 import OnePlatform from "./components/one-platform";
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Hero />
       <OnePlatform />
+      <Brouchst />
       <SuccessServicesSection />
     </div>
   );
